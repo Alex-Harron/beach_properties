cf80c38990371d2a9d30a26e06e37c2891d1cdf8
Alex Harron <aharron.harron@gmail.com>
2021-06-22 12:48:52 -0400

    first commit

---
+++ db/migrate/20210621173229_create_houses.rb
@@ -0,0 +1,11 @@
+class CreateHouses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.string :description
+      t.string :img_url
+
+      t.timestamps
+    end
+  end
+end
